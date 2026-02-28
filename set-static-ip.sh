f2ee2718bef7ca938907ec6e5da501869ead01e9
abdulazizax <phoenixaax2011@gmail.com>
2026-02-28 19:38:45 +0500

    feat: set-static-ip.sh file edited 1.2

--- set-static-ip.sh
+++ set-static-ip.sh
@@ -1,27 +1,14 @@
-#!/bin/bash
+# 1. Custom netplan configni o'chirish
+sudo rm -f /etc/netplan/01-static-ip.yaml
 
-# Usage: sudo ./set-static-ip.sh 192.168.1.50
-
-NEW_IP="${1:-192.168.1.50}"
-
-echo "🔧 Setting IP: $NEW_IP"
-
-sudo tee /etc/netplan/01-static-ip.yaml > /dev/null <<EOF
+# 2. Default configni tiklash (NetworkManager orqali DHCP)
+sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null <<EOF
 network:
   version: 2
-  renderer: networkd
-  ethernets:
-    enp1s0:
-      dhcp4: no
-      addresses:
-        - $NEW_IP/24
-      nameservers:
-        addresses:
-          - 8.8.8.8
+  renderer: NetworkManager
 EOF
+sudo chmod 600 /etc/netplan/01-network-manager-all.yaml
 
-sudo chmod 600 /etc/netplan/01-static-ip.yaml
+# 3. Qo'llash
 sudo netplan apply
-
-echo "✅ Done! IP: $NEW_IP"
-ip addr show enp1s0 | grep "inet "
+sudo systemctl restart NetworkManager
